4c1cf211cca0b51fe545a01939ff8cf9ab1bc5d7
Jun <604803231@qq.com>
2023-03-03 20:46:04 +0800

    新增图像膨胀腐蚀

--- JuDevice/OpenCV/Opencv_Blur_Pack/opencv_blur.py
+++ JuDevice/OpenCV/Opencv_Blur_Pack/opencv_blur.py
@@ -4,7 +4,8 @@
 # @Author  : Jun_军
 # @File    : opencv_bgr_split.py
 
-from cv2 import blur
+from cv2 import blur, erode, dilate
+from numpy import uint8, ones
 
 
 class JuOpencvBlur(object):
@@ -15,6 +16,26 @@ class JuOpencvBlur(object):
         try:
             ksize = int(ksize)
             img = blur(img, (ksize, ksize))
+            result = [True, [img], None, None]
+        except BaseException as e:
+            result = [False, e, None, None]
+        return result
+
+    def opencv_erode_func(self, img, ksize, num):
+        try:
+            ksize = int(ksize)
+            kernel = ones(shape=[ksize, ksize], dtype=uint8)
+            img = erode(img, kernel=kernel, iterations=int(num))
+            result = [True, [img], None, None]
+        except BaseException as e:
+            result = [False, e, None, None]
+        return result
+
+    def opencv_dilate_func(self, img, ksize, num):
+        try:
+            ksize = int(ksize)
+            kernel = ones(shape=[ksize, ksize], dtype=uint8)
+            img = dilate(img, kernel, iterations=int(num))
             result = [True, [img], None, None]
         except BaseException as e:
             result = [False, e, None, None]
