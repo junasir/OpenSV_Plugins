b54d60d150da810564f196bbe7698860506a1960
Jun <604803231@qq.com>
2023-03-07 21:12:11 +0800

    新增仿射变换

--- JuDevice/OpenCV/Opencv_Turn_Over_Pack/opencv_turn_over.py
+++ JuDevice/OpenCV/Opencv_Turn_Over_Pack/opencv_turn_over.py
@@ -3,8 +3,9 @@
 # @Time    : 2023/3/1 22:05
 # @Author  : Jun_军
 # @File    : opencv_turn_over.py
-
-from cv2 import flip
+from copy import deepcopy
+from cv2 import flip, warpAffine
+from numpy import float32
 
 
 class JuOpencvTurnOver(object):
@@ -15,6 +16,17 @@ class JuOpencvTurnOver(object):
         try:
             flip_img = flip(img, int(num))
             result = [True, [flip_img], None, None]
+        except BaseException as e:
+            result = [False, e, None, None]
+        return result
+
+    def opencv_imitation_func(self, img, num1, num2, num3, num4, num5, num6):
+        try:
+            rows, cols, channel = img.shape
+            M = float32([[float(num1), float(num2), float(num3)],
+                         [float(num4), float(num5), float(num6)]])
+            dst = warpAffine(deepcopy(img), M=M, dsize=(cols, rows))
+            result = [True, [dst], None, None]
         except BaseException as e:
             result = [False, e, None, None]
         return result
